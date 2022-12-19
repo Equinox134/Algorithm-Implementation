71c64828f0114bb605275b2dc2a17bea3f00e95e
Equinox134 <103185023+Equinox134@users.noreply.github.com>
2022-12-19 14:17:36 +0900

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+#define fastio cin.tie(0)->sync_with_stdio(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define ff first
+#define ss second
+#define INF 0x7f7f7f7f
+#define MAX 500010
+
+int main(){
+	fastio;
+	
+}
