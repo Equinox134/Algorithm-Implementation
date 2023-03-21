a09f9a7a259c0325b540ad7d05c2990133f9a8dc
Equinox134 <103185023+Equinox134@users.noreply.github.com>
2023-03-21 22:06:39 +0900

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -6,10 +6,12 @@ typedef pair<ll,ll> pll;
 typedef pair<double,double> pdd;
 #define fastio cin.tie(0)->sync_with_stdio(0); cout.tie(0);
 #define all(x) x.begin(),x.end()
+#define compress(x) x.erase(unique(all(x)),x.end())
 #define ff first
 #define ss second
-#define INF 0x7f7f7f7f
+#define INF 987654321
 #define MAX 500010
+#define SIZE 100010
 
 int main(){
 	fastio;
